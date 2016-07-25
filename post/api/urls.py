70b5380ca66ea6e16e7c41d303f3c68ba3317ded
uiandwe <uiandwe@gmail.com>
2016-07-25 22:53:07 +0900

    api/post update, delete

--- post/api/urls.py
+++ post/api/urls.py
@@ -6,12 +6,17 @@ from django.contrib import admin
 from .views import (
     PostDetailAPIView,
     PostListAPIView,
+    PostUpdateAPIView,
+    PostDeleteAPIView,
+
 )
 
 
 urlpatterns = [
     url(r'^$', PostListAPIView.as_view(), name='list'),
     url(r'^(?P<pk>\d+)/$', PostDetailAPIView.as_view(), name='detail'),
+    url(r'^(?P<pk>\d+)/edit/$', PostUpdateAPIView.as_view(), name='update'),
+    url(r'^(?P<pk>\d+)/delete/$', PostDeleteAPIView.as_view(), name='delete'),
 
 
 ]
--- post/api/views.py
+++ post/api/views.py
@@ -1,5 +1,5 @@
 __author__ = 'uiandwe'
-from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView
 
 from post.models import Post
 from post.api.serializers import PostSerializer
@@ -9,6 +9,17 @@ class PostListAPIView(ListAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+
 class PostDetailAPIView(RetrieveAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostUpdateAPIView(UpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class PostDeleteAPIView(DestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer

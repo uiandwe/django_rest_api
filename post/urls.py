f3076f08797649fe4f277d2726c996e9151a2ad2
uiandwe <uiandwe@gmail.com>
2016-08-18 21:29:04 +0900

    post generic detail  template

--- post/urls.py
+++ post/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     url(regex=r"^$", view=views.PostListView.as_view(), name="list"),
+    url(regex=r"^(?P<pk>\d+)/$", view=views.PostDetailView.as_view(), name="detail"),
 ]
